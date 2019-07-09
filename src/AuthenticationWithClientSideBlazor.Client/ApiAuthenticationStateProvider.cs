7cb325a1ad2dcccc124db596f23dcad504197d2f
Chris Sainty <chrissainty@users.noreply.github.com>
2019-07-10 00:16:13 +0100

    Initial commit

---
+++ src/AuthenticationWithClientSideBlazor.Client/ApiAuthenticationStateProvider.cs
@@ -0,0 +1,53 @@
+﻿using AuthenticationWithClientSideBlazor.Shared;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthenticationWithClientSideBlazor.Client
+{
+    public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        public ApiAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var savedToken = await _localStorage.GetItemAsync<string>("authToken");
+
+            if (!string.IsNullOrWhiteSpace(savedToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
+            }
+
+            var userInfo = await _httpClient.GetJsonAsync<UserModel>("api/accounts/user");
+
+            var identity = userInfo.IsAuthenticated
+                ? new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userInfo.Email) }, "apiauth")
+                : new ClaimsIdentity();
+
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+
+        public void MarkUserAsAuthenticated(string email)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}
---
+++ src/AuthenticationWithClientSideBlazor.Client/Services/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using AuthenticationWithClientSideBlazor.Shared;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AuthenticationWithClientSideBlazor.Client.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthService(HttpClient httpClient,
+                           AuthenticationStateProvider authenticationStateProvider,
+                           ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _authenticationStateProvider = authenticationStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<RegisterResult> Register(RegisterModel registerModel)
+        {
+            var result = await _httpClient.PostJsonAsync<RegisterResult>("api/accounts", registerModel);
+
+            return result;
+        }
+
+        public async Task<LoginResult> Login(LoginModel loginModel)
+        {
+            var result = await _httpClient.PostJsonAsync<LoginResult>("api/Login", loginModel);
+
+            if (result.Successful)
+            {
+                await _localStorage.SetItemAsync("authToken", result.Token);
+                ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.Email);
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Token);
+
+                return result;
+            }
+
+            return result;
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsLoggedOut();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
